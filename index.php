97ef3cc95e28a13b93008b43da08302807a7b3bb
jonhylafarm <JonhyLaFarm@MacBook-Pro-de-Jonathan.local>
2013-10-02 13:17:11 -0500

    Meta datos
    Meta datos para el index, aun falta rellenarlos

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico">
 <title> &lt; Slash/Inn &gt; - Soluciones en TI</title>
 </head>
 
