140c0a3440d0f1f7f4275716c797cac93cf22647
jonhylafarm <JonhyLaFarm@MacBook-Pro-de-Jonathan.local>
2013-10-25 10:56:27 -0500

    Nosotros
    Archivo nuevo de nosotros

---
+++ nosotros.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico">
+<title> &lt; Slash/Inn &gt; - Soluciones en TI</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12">&nbsp;</div>
+        	<div class="col-md-3"><a href="index.php" class="text-center"><img src="img/logo.png" width="100%"></a></div>
+            <div class="col-md-9">&nbsp;</div>
+            <div class="col-md-5 col-md-offset-4 text-right visible-lg">
+            		<a class="btn btn-default">Inicio</a>&nbsp;
+                    <a class="btn btn-default">Portafolio</a>&nbsp;
+                    <a class="btn btn-default">Nosotros</a>&nbsp;
+                    <a class="btn btn-default">Contacto</a>&nbsp;
+            </div>
+            <div class="col-md-5 col-md-offset-4 text-center hidden-lg">
+            		<a class="btn btn-default">Inicio</a>&nbsp;
+                    <a class="btn btn-default">Portafolio</a>&nbsp;
+                    <a class="btn btn-default">Nosotros</a>&nbsp;
+                    <a class="btn btn-default">Contacto</a>&nbsp;
+            </div>
+        </div>
+        <hr>
+        <hr style="margin-top:-10px;">
+        
+        <hr>
+        <div class="row">
+        	<div class="col-md-12" style="margin:-5px 0 10px 0;">
+        		<img src="img/twitter.png" height="40" width="40">&nbsp;&nbsp;
+                <img src="img/FB.png" height="40" width="40">
+            	<span class="pull-right" style=" margin-top:10px;">Slash Inn 2013 &copy;</span>
+        	</div>
+    </div>
+	<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
