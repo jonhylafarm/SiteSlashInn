76e4aff61e0d67eaa4121cfadc1baaff273d13de
jonhylafarm <JonhyLaFarm@MacBook-Pro-de-Jonathan.local>
2013-10-27 21:46:56 -0600

    Contacto
    Formulario de contacto, creo deberia tener tambien medios de contacto,
    como twitter y facebook.

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="img/favicon.ico">
+<title> &lt; Slash/Inn &gt; - Soluciones en TI - Contacto</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="container">
+    </div>
+	<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
